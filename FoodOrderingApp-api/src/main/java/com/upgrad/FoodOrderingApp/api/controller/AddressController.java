e5ea3a87de9283d7eb6e8b51094c8697e5b518f6
HemaprabhaAshwin <hemaprabha.ashwin@gmail.com>
2019-12-06 19:22:05 +0400

    Incorporated logic for the endpoint get All saved addresses of the customer

--- FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/AddressController.java
+++ FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/AddressController.java
@@ -4,6 +4,9 @@ import com.upgrad.FoodOrderingApp.service.businness.AddressService;
 import com.upgrad.FoodOrderingApp.service.businness.CustomerService;
 import com.upgrad.FoodOrderingApp.api.model.SaveAddressResponse;
 import com.upgrad.FoodOrderingApp.api.model.SaveAddressRequest;
+import com.upgrad.FoodOrderingApp.api.model.AddressListResponse;
+import com.upgrad.FoodOrderingApp.api.model.AddressList;
+import com.upgrad.FoodOrderingApp.api.model.AddressListState;
 import com.upgrad.FoodOrderingApp.service.entity.AddressEntity;
 import com.upgrad.FoodOrderingApp.service.entity.CustomerAddressEntity;
 import com.upgrad.FoodOrderingApp.service.entity.CustomerEntity;
@@ -17,6 +20,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -29,7 +33,7 @@ public class AddressController {
     @Autowired
     private CustomerService customerService;
 
-    @RequestMapping(value = "/address", method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @RequestMapping(value = "/address", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<SaveAddressResponse> saveAddress(@RequestBody(required = false) SaveAddressRequest saveAddressRequest,
                                                            @RequestHeader("authorization") final String accessToken) throws AuthorizationFailedException, SaveAddressException, AddressNotFoundException {
         String[] bearerToken = accessToken.split("Bearer ");
@@ -55,5 +59,35 @@ public class AddressController {
                 .id(persistedAddressEntity.getUuid()).status("ADDRESS SUCCESSFULLY REGISTERED");
 
         return new ResponseEntity<SaveAddressResponse>(saveAddressResponse, HttpStatus.CREATED);
+    }
+
+
+    @RequestMapping(value = "/address/customer", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<AddressListResponse> getSavedAddresses(@RequestHeader("authorization") final String accessToken) throws AuthorizationFailedException {
+        String[] bearerToken = accessToken.split("Bearer ");
+        final CustomerEntity customerEntity = customerService.getCustomer(bearerToken[1]);
+        final List<CustomerAddressEntity> listCustomerAddressEntity = addressService.getCustomerAddressesByCustomer(customerEntity);
+
+        AddressListResponse addressListResponse = new AddressListResponse();
+
+        for(CustomerAddressEntity customerAddressEntity : listCustomerAddressEntity ){
+            AddressEntity addressEntity = customerAddressEntity.getAddress();
+            AddressList addressList = new AddressList();
+            addressList.setId(UUID.fromString(addressEntity.getUuid()));
+            addressList.setFlatBuildingName(addressEntity.getFlatBuilNumber());
+            addressList.setLocality(addressEntity.getLocality());
+            addressList.setPincode(addressEntity.getPinCode());
+
+            StateEntity stateEntity = addressEntity.getState();
+            AddressListState addressListState = new AddressListState();
+            addressListState.setId(UUID.fromString(stateEntity.getUuid()));
+            addressListState.setStateName(stateEntity.getStateName());
+
+            addressList.state(addressListState);
+
+            addressListResponse.addAddressesItem(addressList);
+
+        }
+        return new ResponseEntity<AddressListResponse>(addressListResponse, HttpStatus.OK);
     }
 }
--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/CustomerAddressDao.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/CustomerAddressDao.java
@@ -2,11 +2,13 @@ package com.upgrad.FoodOrderingApp.service.dao;
 
 import com.upgrad.FoodOrderingApp.service.entity.AddressEntity;
 import com.upgrad.FoodOrderingApp.service.entity.CustomerAddressEntity;
+import com.upgrad.FoodOrderingApp.service.entity.CustomerEntity;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Repository
 public class CustomerAddressDao {
@@ -19,6 +21,15 @@ public class CustomerAddressDao {
         try{
             return this.entityManager.createNamedQuery("customerAddressByAddressId", CustomerAddressEntity.class).setParameter("address", addressEntity).getSingleResult();
         }catch(NoResultException nre){
+            return null;
+        }
+    }
+
+    //Retrieve the list of addresses of a particular customer
+    public List<CustomerAddressEntity> getCustomerAddressListByCustomer(CustomerEntity customerEntity){
+        try{
+            return this.entityManager.createNamedQuery("customerAddressesByCustomerId", CustomerAddressEntity.class).setParameter("customer", customerEntity).getResultList();
+        }catch (NoResultException nre){
             return null;
         }
     }
