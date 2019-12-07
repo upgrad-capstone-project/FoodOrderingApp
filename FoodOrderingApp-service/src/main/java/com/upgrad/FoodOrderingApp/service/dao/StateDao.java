a29c5c5b38f46fd304b1c34f0e59e442f7d122bc
HemaprabhaAshwin <hemaprabha.ashwin@gmail.com>
2019-12-07 11:05:43 +0400

    Incorporated logic for the endpoint All states

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/StateDao.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/StateDao.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Repository
 public class StateDao {
@@ -20,4 +21,13 @@ public class StateDao {
             return null;
         }
     }
+
+    public List<StateEntity> getAllStates(){
+        try{
+            return this.entityManager.createNamedQuery("allTheStates", StateEntity.class).getResultList();
+        }catch(NoResultException nre){
+            return null;
+        }
+    }
+
 }
