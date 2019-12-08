fb88c270ac907a5136ec6c7981e7200470f267b5
HemaprabhaAshwin <hemaprabha.ashwin@gmail.com>
2019-12-08 11:06:38 +0400

    Logic for Payment Controller

---
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/RestaurantEntity.java
@@ -0,0 +1,12 @@
+package com.upgrad.FoodOrderingApp.service.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(
+        name = "restaurant"
+)
+public class RestaurantEntity {
+
+}
